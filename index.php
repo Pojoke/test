3b4782fa030bb01b6683b106cd9417251fdb6a02
Pojoke <pavlykdmutro@gmail.com>
2024-09-12 19:26:39 +0300

    php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Php </h1>
+    <?php
+    $year = 2024;//inteleger
+    $is_hungry = true;//boolean
+    $price = 3.14;//flout
+    $favorite_movie = "Острів скарбів"; //string
+    echo "$year, $is_hungry , $price, $favorite_movie"
+        ?>
+</body>
+
+</html>
